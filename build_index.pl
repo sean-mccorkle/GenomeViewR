e47bc0b8922aef3a57a11df5514f3f4e93d9516a
Sean R. McCorkle <sean.r.mccorkle@gmail.com>
2016-04-14 11:37:05 -0400

    added line number to build_index.pl, and comments

--- build_index.pl
+++ build_index.pl
@@ -1,8 +1,21 @@
 #!/usr/bin/env perl
+# Program:      build_index.pl
+# Programmer:   Sean R. McCorkle
+#               Biology Departmnet, Brookhaven National Laboratory
+# Language:     perl
 #
-# create index of samfile for seek() operations
-# 
-#  chromosome name is in column 2 (counting from 0), position is column 3
+# Description:  Creates an index of blocks of SAMfile for seek() operations,
+#               to allow rapid retreival of 1 megabyte regions surrounding
+#               a given chromosome/position.
+#
+# Usage:        build_index.pl <SAMfile> >output_index
+#
+#               where <SAMfile> is a sorted SAM-format filey
+#
+#               output (on stdout) is an ascii, where each record contains
+#
+#               <chrom> <beginning_position_of_block> <line number> <file position>
+#
 #
 use strict;
 
@@ -14,10 +27,14 @@ my $last_chrom_pos = 0;
 
 open( SAM, $samfile ) || die "can't open $samfile: $!\n";
 
+my $line_num = 0;           # for now, first line is numbered 0
 my $file_pos = tell( SAM );
 
 while ( $_ = <SAM> )
    {
+    # Reading the input SAM file, its assumed that chromosome 
+    # name is in column 2 (counting from 0), position is column 3
+
     my ( $chrom, $chrom_pos ) = (split( /\t/ ))[2,3];
 
     if ( ($chrom ne $last_chrom) )
@@ -27,9 +44,10 @@ while ( $_ = <SAM> )
           }
     while ( $last_chrom_pos <= $chrom_pos )
        {
-        print "$chrom $last_chrom_pos $file_pos\n";
+        print "$chrom $last_chrom_pos $line_num $file_pos\n";
         $last_chrom_pos += $blocksize;
        }
     $file_pos = tell( SAM );
+    $line_num++;
    }
 close( SAM );
